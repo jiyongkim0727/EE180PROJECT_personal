39239d25a0225d247a1646496e17e56797abe45a
root <junwyoo@github.com>
2017-04-12 14:16:01 -0700

    add mean and peak detection

--- jun_codes/main.c
+++ jun_codes/main.c
@@ -22,8 +22,105 @@ double *t, *t1,*t2;
 float *x, *y, *z,*gx,*gy,*gz;
 float *S_i;
 int n_S;
+float PK_TH = 0.1;
 
+/*
+ * sets first <n> values in <*arr> to <val>
+ */
+void clear_buffer(float *arr, float val, int n) 
+{
+	int i;
+	for (i = 0; i < n; i++) {
+		arr[i] = val;
+	}
+}
 
+/*
+ * Caculates mean of first <n> samples in <*arr>
+ */
+float calculate_mean(float *arr, int n)
+{
+	float total;
+	int i;
+
+	total = 0.0f;
+	for (i = 0; i < n; i++) {
+		total += arr[i];
+	}
+
+	return total/((float) n);
+}
+
+int 
+find_peaks_and_troughs(
+		float *arr, 	// signal 
+		int n_samples, 	// number of samples present in the signal
+		// arrays that will store the indicies of the located
+		// peaks and troughs
+		float *P, float *T,
+		// number of peaks (n_P) and number of troughs (n_T)
+		// found in the data set *arr
+		int *n_P, int *n_T
+		)
+{
+	int a, b, i, d, _n_P, _n_T;
+	float E = PK_TH;
+	i = -1; d = 0; a = 0; b = 0;
+	_n_P = 0; _n_T = 0;
+
+	clear_buffer(P, 0.0f, n_samples);
+	clear_buffer(T, 0.0f, n_samples);
+
+	while (i != n_samples) {
+		i++;
+		if (d == 0) {
+			if (arr[a] >= (arr[i] + E)) {
+				d = 2;
+			} else if (arr[i] >= (arr[b] + E)) {
+				d = 1;
+			}
+			if (arr[a] <= arr[i]) {
+				a = i;
+			} else if (arr[i] <= arr[b]) {
+				b = i;
+			}
+		} else if (d == 1) {
+			if (arr[a] <= arr[i]) {
+				a = i;
+			} else if (arr[a] >= (arr[i] + E)) {
+				/*
+				 * Peak has been detected.
+				 * Add index at detected peak
+				 * to array of peak indicies
+				 * increment count of peak indicies
+				 */
+				P[_n_P] = a;
+				_n_P++;
+				b = i;
+				d = 2;
+			}
+		} else if (d == 2) {
+			if (arr[i] <= arr[b]) {
+				b = i;
+			} else if (arr[i] >= (arr[b] + E)) {
+				/*
+				 * Trough has been detected.
+				 * Add index at detected trough
+				 * to array of trough indicies
+				 * increment count of trough indicies
+				 */
+				T[_n_T] = b;
+				_n_T++;
+				a = i;
+				d = 1;
+			}
+		}
+	}
+
+	(*n_P) = _n_P;
+	(*n_T) = _n_T;
+	return 0;
+}
 
 //[FUNCTIONS]
 
@@ -89,9 +186,6 @@ void get_input_file()
 
 	return;
 }
-int detect_stair_descend(float* gz_arr)
-{
-}
 
 void write_to_file()
 {
@@ -142,6 +236,10 @@ void train_data(char* input_file, const unsigned int n_input,
 	return;
 }
 
+int detect_stair_descend(float* gz_arr)
+{
+}
+
 int main(int argc, char **argv)
 {
 	/*[ARGUMENT CHECKS]*/
@@ -160,8 +258,7 @@ int main(int argc, char **argv)
 
 	/*[DETECTION ALGORITHM]*/
 	detect_stair_descend(gz);
-
-
+	
 	/*[WRITING]*/
 	write_to_file();
 
@@ -172,3 +269,4 @@ int main(int argc, char **argv)
 
 	exit(0);
 }
+
